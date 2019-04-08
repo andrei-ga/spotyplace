19d7a14960fdd2ae3305b0603586cc46e26eccbc
Andrei Georgescu <ice_howler@yahoo.com>
2019-04-08 10:12:26 +0300

    Update Startup.cs

--- Server/Spotyplace.Web/Startup.cs
+++ Server/Spotyplace.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
