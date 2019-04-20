6fd82dad32f05a07e670c0c00e498c5c75016c26
Andrei Georgescu <ice_howler@yahoo.com>
2019-04-20 22:55:26 +0300

    Report improvement

--- Server/Spotyplace.Business/Managers/ReportManager.cs
+++ Server/Spotyplace.Business/Managers/ReportManager.cs
@@ -23,7 +23,7 @@ namespace Spotyplace.Business.Managers
         public async Task ReportItem(ReportReasonEnum reportReason, string url, string userEmail)
         {
             string title = "[Report] Item reported";
-            string emailBody = string.Format("The following item has been reported<br><br><a href='{0}'>{0}</a><br><br>User who reported the item: ", url);
+            string emailBody = string.Format("The following item has been reported<br><br><a href='{0}'>{0}</a><br><br>Reason for the report: {1}<br><br>User who reported the item: ", url, reportReason.ToString());
             emailBody += string.IsNullOrEmpty(userEmail) ? "anonymous" : userEmail;
 
             await _emailService.SendEmail(title, emailBody, _adminOptions.Users);
