d1ddb8a3fc8c2306d7bd15418b642514ea046860
Andrei Georgescu <ice_howler@yahoo.com>
2019-06-29 16:07:29 +0300

    Refactor search page

--- Client/src/app/main/main-routing.module.ts
+++ Client/src/app/main/main-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { CookiesDeclarationComponent } from './components/cookies-declaration/cookies-declaration.component';
 import { PricingComponent } from './components/pricing/pricing.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
   },
   {
     path: 'cookies',
