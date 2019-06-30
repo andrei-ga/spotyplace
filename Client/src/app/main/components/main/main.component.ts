ab7bd92ca8fc811e7efe0c77b2a637d2ba30ccad
Andrei Georgescu <ice_howler@yahoo.com>
2019-06-30 21:18:57 +0300

    Homepage header

--- Client/src/app/main/components/main/main.component.ts
+++ Client/src/app/main/components/main/main.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { AppConfigService } from '../../../shared/services/app-config.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent {
+  cdnBasePath: string;
+
+  constructor(private appConfigService: AppConfigService) {
+    this.cdnBasePath = this.appConfigService.getConfig().CDN_BASE_PATH;
+  }
+}
