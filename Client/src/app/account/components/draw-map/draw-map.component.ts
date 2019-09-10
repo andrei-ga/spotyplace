b171e1d083ef21e63bdd3ea6d1f34a5d77b2be0b
Andrei Georgescu <ice_howler@yahoo.com>
2019-09-10 22:06:48 +0300

    Draw map

--- Client/src/app/account/components/draw-map/draw-map.component.ts
+++ Client/src/app/account/components/draw-map/draw-map.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, HostListener, NgZone, OnDestroy, ViewChild } from '@angular/core';
+import { MapInfo } from '../../../shared/models/map-info';
 
 declare var PIXI: any;
 
@@ -13,11 +14,73 @@ export class DrawMapComponent implements AfterViewInit, OnDestroy {
 
   public pApp: any;
 
+  pixiLoaded = false;
+
+  drawingWall = false;
+
+  drawingGraphic;
+
+  drawingLine;
+
+  mapInfo: MapInfo = new MapInfo();
+
+  initialPointerLocation;
+
   constructor(private ngZone: NgZone) {}
 
   @HostListener('window:resize', ['$event'])
   scrollHandler(event) {
-    this.pApp.renderer.resize(this.pixiContainer.nativeElement.clientWidth, (window as any).innerHeight - 120);
+    this.resizePixi();
+  }
+
+  setIsDrawingWall(value: boolean) {
+    this.ngZone.run(() => {
+      this.drawingWall = value;
+    });
+
+    if (value) {
+      this.drawingGraphic = new PIXI.Graphics();
+      this.drawingGraphic.interactive = true;
+      this.drawingGraphic.hitArea = new PIXI.Rectangle(0, 0, window.innerWidth, window.innerHeight);
+
+      this.drawingLine = undefined;
+
+      this.drawingGraphic.on('pointermove', (event) => {
+        if (this.drawingLine) {
+          const mouseX = event.data.global.x;
+          const mouseY = event.data.global.y;
+
+          this.drawingLine.clear();
+          this.drawingLine.lineStyle(10, 0x000000, 1);
+          this.drawingLine.moveTo(this.initialPointerLocation[0], this.initialPointerLocation[1]);
+          this.drawingLine.lineTo(
+            Math.abs(this.initialPointerLocation[0] - mouseX) < 10 ? this.initialPointerLocation[0] : mouseX,
+            Math.abs(this.initialPointerLocation[1] - mouseY) < 10 ? this.initialPointerLocation[1] : mouseY
+          );
+        }
+      });
+
+      this.drawingGraphic.on('pointerdown', (event) => {
+        const mouseX = event.data.global.x;
+        const mouseY = event.data.global.y;
+        this.initialPointerLocation = [mouseX, mouseY];
+
+        if (this.drawingLine) {
+          this.mapInfo.lines.push({ ...this.drawingLine });
+          this.drawingLine = undefined;
+        } else {
+          this.drawingLine = new PIXI.Graphics();
+          this.drawingLine.lineStyle(10, 0x000000, 1);
+          this.drawingLine.moveTo(mouseX, mouseY);
+          this.pApp.stage.addChild(this.drawingLine);
+        }
+      });
+
+      this.pApp.stage.addChild(this.drawingGraphic);
+    } else {
+      this.pApp.stage.removeChild(this.drawingGraphic);
+      this.drawingGraphic = undefined;
+    }
   }
 
   ngAfterViewInit(): void {
@@ -25,13 +88,20 @@ export class DrawMapComponent implements AfterViewInit, OnDestroy {
       this.ngZone.runOutsideAngular(() => {
         this.pApp = new PIXI.Application({
           autoResize: true,
+          backgroundColor: 0xffffff,
           width: this.pixiContainer.nativeElement.clientWidth,
-          height: (window as any).innerHeight - 120,
+          height: window.innerHeight - 120,
         });
       });
 
+      this.pApp.stage.interactive = true;
       this.pixiContainer.nativeElement.appendChild(this.pApp.view);
-    }, 500);
+      this.pixiLoaded = true;
+    }, 100);
+  }
+
+  resizePixi() {
+    this.pApp.renderer.resize(this.pixiContainer.nativeElement.clientWidth, window.innerHeight - 120);
   }
 
   ngOnDestroy(): void {
